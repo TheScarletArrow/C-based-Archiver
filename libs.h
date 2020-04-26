8c831a303d07bfc5424e8c2f36a00d641c95ffcf
TheScarletArrow <anton20001701@yandex.ru>
2020-04-26 19:17:50 +0300

    added libraries
    Signed-off-by: TheScarletArrow <anton20001701@yandex.ru>

--- libs.h
+++ libs.h
@@ -4,5 +4,19 @@
 
 #ifndef C_BASED_ARCHIVER_LIBS_H
 #define C_BASED_ARCHIVER_LIBS_H
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#define LengthMax 256
+#define bufsize 1024
+#define ArgsMin 2
 
 #endif //C_BASED_ARCHIVER_LIBS_H
